993ff8d4cd8ead9785c3d478231e9d5b29fe58ab
Tom Alexa <tom.alexa.fin@gmail.com>
2021-08-11 21:03:47 +0200

    Add 2020 > 05
    Add 2020 > 05

---
+++ 2020/05/main.py
@@ -0,0 +1,82 @@
+import time
+
+# --- Day 5: Binary Boarding ---
+
+
+def get_data_from_input():
+    binary_seats = set()
+    with open("2020/05/input", "r") as file:
+        for line in file.readlines():
+            seat = line.strip()
+            binary_seats.add(seat)
+    return binary_seats
+
+
+def get_seats_and_max_min_id(binary_seats):
+    seats = {}
+    max_id = 0
+    min_id = float("inf")
+    for binary_seat in binary_seats:
+        row = 0
+        column = 0
+        for i, letter in enumerate(binary_seat):
+            if letter == "B":
+                row += 2**(6 - i)
+            elif letter == "R":
+                column += 2**(2 - (i - 7))
+        seat_id = (row * 8) + column
+        seats[seat_id] = {"row": row, "column": column}
+        if seat_id > max_id:
+            max_id = seat_id
+        elif seat_id < min_id:
+            min_id = seat_id
+    return {"seats": seats, "max_id": max_id, "min_id": min_id}
+
+
+def get_answer_1(binary_seats):
+    time_start = time.perf_counter()
+    max_id = get_seats_and_max_min_id(binary_seats)["max_id"]
+    time_spent = time.perf_counter() - time_start
+    return {"value": max_id, "time": time_spent}
+
+
+def get_answer_2(binary_seats):
+    time_start = time.perf_counter()
+    database = get_seats_and_max_min_id(binary_seats)
+    my_seat_id = None
+    for seat_id in range(database["min_id"], database["max_id"] + 1):
+        try:
+            _ = database["seats"][seat_id]
+        except KeyError:
+            my_seat_id = seat_id
+            break
+    time_spent = time.perf_counter() - time_start
+    return {"value": my_seat_id, "time": time_spent}
+
+
+def print_answers(answer_1, answer_2):
+    to_miliseconds = 1000
+    answer_1_value = answer_1["value"]
+    answer_2_value = answer_2["value"]
+    answer_1_time = round(answer_1["time"] * to_miliseconds, 3)
+    answer_2_time = round(answer_2["time"] * to_miliseconds, 3)
+
+    to_add = abs(len(str(answer_1_value)) - len(str(answer_2_value)))
+    lenght_to_add_1 = " " * to_add if len(str(answer_1_value)) < len(str(answer_2_value)) else " " * 0
+    lenght_to_add_2 = " " * to_add if len(str(answer_1_value)) > len(str(answer_2_value)) else " " * 0
+
+    indetation = " " * 2
+    print(f"\n{indetation}2020 > 05")
+    print(f"{indetation*2}Answer 1: {answer_1_value}{lenght_to_add_1} | {answer_1_time:.3f} ms")
+    print(f"{indetation*2}Answer 2: {answer_2_value}{lenght_to_add_2} | {answer_2_time:.3f} ms\n")
+
+
+def main():
+    puzzle_input = get_data_from_input()
+    answer_1 = get_answer_1(puzzle_input)
+    answer_2 = get_answer_2(puzzle_input)
+    print_answers(answer_1, answer_2)
+
+
+if __name__ == "__main__":
+    main()
--- days.py
+++ days.py
@@ -15,7 +15,7 @@ DAY_INDENTATION = " " * 8
 # days
 COMPLETED_DAYS = {
     2020: {
-        1, 2, 3,
+        1, 2, 3, 4, 5,
     },
 }
 
