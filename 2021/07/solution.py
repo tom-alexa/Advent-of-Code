e09102580536dee59e3d5a3bf4d5b7bd8b60bbf2
Tom Alexa <tom.alexa.fin@gmail.com>
2021-12-07 08:51:44 +0100

    Add 2021 > 07
    Add 2021 > 07

--- 2021/07/solution.py
+++ 2021/07/solution.py
@@ -1,8 +1,7 @@
 import time
 from pathlib import PurePath
-from collections import Counter
 
-# --- Day 6: Lanternfish ---
+# --- Day 7: The Treachery of Whales ---
 
 
 ###############
@@ -10,7 +9,7 @@ from collections import Counter
 ###############
 
 YEAR = 2021
-DAY = 6
+DAY = 7
 
 INPUT_FILE = PurePath(f"{YEAR:04}/{DAY:02}/input.txt")
 
@@ -21,43 +20,44 @@ INPUT_FILE = PurePath(f"{YEAR:04}/{DAY:02}/input.txt")
 
 def get_data_from_input():
     with open(INPUT_FILE, "r") as file:
-        return dict(Counter([int(number) for number in file.read().split(",")]))
+        return [int(number) for number in file.read().split(",")]
 
 
 ###############
 #  functions  #
 ###############
 
-def growth(initial_state, days):
-    list_of_lanternfish = initial_state.copy()
-    for _ in range(days):
-        new_fish = {i: 0 for i in range(9)}
-        for timer, count in list_of_lanternfish.items():
-            timer -= 1
-            if timer < 0:
-                new_fish[8] += count
-                timer = 6
-            new_fish[timer] += count
-        list_of_lanternfish = new_fish.copy()
-    return sum(list_of_lanternfish.values())
+def least_fuel(position, fuel_growth=False):
+    minimal = min(position)
+    maximal = max(position)
+    best = float("inf")
+    for new in range(minimal, maximal + 1):
+        current = 0
+        for pos in position:
+            change = abs(pos - new)
+            if fuel_growth:
+                change += ((change - 1) * 0.5 * change)
+            current += change
+        best = min(best, current)
+    return int(best)
 
 
 #############
 #  answers  #
 #############
 
-def get_answer_1(initial_state):
+def get_answer_1(position):
     time_start = time.perf_counter()
-    total = growth(initial_state, 80)
+    fuel_cost = least_fuel(position)
     time_spent = time.perf_counter() - time_start
-    return {"value": total, "time": time_spent}
+    return {"value": fuel_cost, "time": time_spent}
 
 
-def get_answer_2(initial_state):
+def get_answer_2(position):
     time_start = time.perf_counter()
-    total = growth(initial_state, 256)
+    fuel_cost = least_fuel(position, fuel_growth=True)
     time_spent = time.perf_counter() - time_start
-    return {"value": total, "time": time_spent}
+    return {"value": fuel_cost, "time": time_spent}
 
 
 ###########
@@ -65,11 +65,10 @@ def get_answer_2(initial_state):
 ###########
 
 def print_answers(answer_1, answer_2):
-    to_milliseconds = 1000
     answer_1_value = answer_1["value"]
     answer_2_value = answer_2["value"]
-    answer_1_time = round(answer_1["time"] * to_milliseconds, 3)
-    answer_2_time = round(answer_2["time"] * to_milliseconds, 3)
+    answer_1_time = round(answer_1["time"], 3)
+    answer_2_time = round(answer_2["time"], 3)
 
     to_add = abs(len(str(answer_1_value)) - len(str(answer_2_value)))
     length_to_add_1 = " " * to_add if len(str(answer_1_value)) < len(str(answer_2_value)) else " " * 0
@@ -77,8 +76,8 @@ def print_answers(answer_1, answer_2):
 
     indentation = " " * 2
     print(f"\n{indentation}{YEAR} > {DAY}")
-    print(f"{indentation*2}Answer 1: {answer_1_value}{length_to_add_1} | {answer_1_time:.3f} ms")
-    print(f"{indentation*2}Answer 2: {answer_2_value}{length_to_add_2} | {answer_2_time:.3f} ms\n")
+    print(f"{indentation*2}Answer 1: {answer_1_value}{length_to_add_1} | {answer_1_time:.3f} s")
+    print(f"{indentation*2}Answer 2: {answer_2_value}{length_to_add_2} | {answer_2_time:.3f} s\n")
 
 
 ##########
